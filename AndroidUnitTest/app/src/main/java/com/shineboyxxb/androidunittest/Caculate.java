87fdb21985b77acbbedd62cccfbcfcfa0b709b9e
xxb <1240806361@qq.com>
2019-05-19 23:35:30 +0800

    完成了test的单元测试和覆盖率报告

---
+++ AndroidUnitTest/app/src/main/java/com/shineboyxxb/androidunittest/Caculate.java
@@ -0,0 +1,12 @@
+package com.shineboyxxb.androidunittest;
+
+public class Caculate {
+
+    public double sum(double firstData, double secondData) {
+        return firstData + secondData;
+    }
+
+    public double substract(double firstData, double secondData) {
+        return firstData - secondData;
+    }
+}
---
+++ AndroidUnitTest/app/src/test/java/com/shineboyxxb/androidunittest/CaculateTest.java
@@ -0,0 +1,31 @@
+package com.shineboyxxb.androidunittest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CaculateTest {
+
+    private Caculate mCalculate;
+
+    @Before
+    public void setUp() throws Exception {
+        mCalculate = new Caculate();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void sum() {
+        assertEquals(6d, mCalculate.sum(1d, 5d), 0);
+    }
+
+    @Test
+    public void substract() {
+        assertEquals(2d, mCalculate.substract(4d, 2d), 0);
+    }
+}
